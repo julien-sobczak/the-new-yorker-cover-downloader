ff6af1b7257de3c977a7a0dba677dc7464c20c1b
Julien Sobczak <julien.sobczak@gmail.com>
2017-07-30 16:15:18 +0200

    List invalid covers

---
+++ jpg2pdf.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#convert cover-192[56789]-*.jpg ../The_New_Yorker-Covers-1925-1930.pdf
+convert cover-193[01234]-*.jpg ../The_New_Yorker-Covers-1930-1935.pdf
+convert cover-193[56789]-*.jpg ../The_New_Yorker-Covers-1935-1940.pdf
+convert cover-194[01234]-*.jpg ../The_New_Yorker-Covers-1940-1945.pdf
+convert cover-194[56789]-*.jpg ../The_New_Yorker-Covers-1945-1950.pdf
+convert cover-195[01234]-*.jpg ../The_New_Yorker-Covers-1950-1955.pdf
+convert cover-195[56789]-*.jpg ../The_New_Yorker-Covers-1955-1960.pdf
+convert cover-196[01234]-*.jpg ../The_New_Yorker-Covers-1960-1965.pdf
+convert cover-196[56789]-*.jpg ../The_New_Yorker-Covers-1965-1970.pdf
+convert cover-197[01234]-*.jpg ../The_New_Yorker-Covers-1970-1975.pdf
+convert cover-197[56789]-*.jpg ../The_New_Yorker-Covers-1975-1980.pdf
+convert cover-198[01234]-*.jpg ../The_New_Yorker-Covers-1980-1985.pdf
+convert cover-198[56789]-*.jpg ../The_New_Yorker-Covers-1985-1990.pdf
+convert cover-199[01234]-*.jpg ../The_New_Yorker-Covers-1990-1995.pdf
+convert cover-199[56789]-*.jpg ../The_New_Yorker-Covers-1995-2000.pdf
+convert cover-200[01234]-*.jpg ../The_New_Yorker-Covers-2000-2005.pdf
+convert cover-200[56789]-*.jpg ../The_New_Yorker-Covers-2005-2010.pdf
+convert cover-201[01234]-*.jpg ../The_New_Yorker-Covers-2010-2015.pdf
+
